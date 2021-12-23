9d96a9baeda17d4cd0845b84edfe00bb62ce7cb9
div-antk <51283468+owaii@users.noreply.github.com>
2021-12-23 21:19:07 +0900

    Initial Commit

---
+++ CarUpkeep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarUpkeep
+//
+//  Created by Takuya Ando on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
