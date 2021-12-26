a5e2600f0f241725cac291658493731f5ee4db78
div-antk <51283468+owaii@users.noreply.github.com>
2021-12-26 21:48:59 +0900

    自動車税のカスタムセルを作成

---
+++ CarUpkeep/AutomobileTaxTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AutomobileTaxTableViewCell.swift
+//  CarUpkeep
+//
+//  Created by Takuya Ando on 2021/12/26.
+//
+
+import UIKit
+
+class AutomobileTaxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
