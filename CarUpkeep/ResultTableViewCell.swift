a721d6af70a28a2f8728d4dbf76d48b171ffb093
div-antk <51283468+owaii@users.noreply.github.com>
2021-12-26 21:53:41 +0900

    金額表示のカスタムセルを作成

---
+++ CarUpkeep/ResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultTableViewCell.swift
+//  CarUpkeep
+//
+//  Created by Takuya Ando on 2021/12/26.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
