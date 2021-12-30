ee288ab1eb7204c0af25a8e1ea2c1b6278ef28a5
div-antk <51283468+owaii@users.noreply.github.com>
2021-12-30 23:16:13 +0900

    車両保険のカスタムセルを作成

---
+++ CarUpkeep/InsuranceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InsuranceTableViewCell.swift
+//  CarUpkeep
+//
+//  Created by Takuya Ando on 2021/12/30.
+//
+
+import UIKit
+
+class InsuranceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
