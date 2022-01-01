8b7b9f697c0c3c41089f35b7b7b5392821e5866a
div-antk <51283468+owaii@users.noreply.github.com>
2022-01-01 18:41:57 +0900

    VCにInstantiateを導入、TableViewの接続

--- CarUpkeep/CalculationViewController.swift
+++ CarUpkeep/CalculationViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Instantiate
 
-class CalculationViewController: UIViewController {
+class CalculationViewController: UIViewController, StoryboardInstantiatable {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -16,6 +17,19 @@ class CalculationViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 270
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        // カスタムセルを登録
+        tableView.register(UINib(nibName: TitleTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: TitleTableViewCell.reusableIdentifier)
+        
+        tableView.register(UINib(nibName: AutomobileTaxTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: AutomobileTaxTableViewCell.reusableIdentifier)
+
+        tableView.register(UINib(nibName: WeightTaxTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: WeightTaxTableViewCell.reusableIdentifier)
+
+        tableView.register(UINib(nibName: ResultTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: ResultTableViewCell.reusableIdentifier)
+        
     }
 
 }
@@ -23,12 +37,60 @@ class CalculationViewController: UIViewController {
 extension CalculationViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 5
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        
+        switch indexPath.item {
+        case 0:
+            guard let titleCell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.reusableIdentifier, for: indexPath) as? TitleTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            titleCell.titleLabel.text = "税金"
+            titleCell.subtitleLabel.text = "絶対にかかるお金です💸"
+            
+            return titleCell
+            
+        case 1:
+            guard let automobileTaxCell = tableView.dequeueReusableCell(withIdentifier: AutomobileTaxTableViewCell.reusableIdentifier, for: indexPath) as? AutomobileTaxTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            return automobileTaxCell
+        
+        case 2:
+            guard let resultCell = tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.reusableIdentifier, for: indexPath) as? ResultTableViewCell else {
+                return UITableViewCell()
+            }
+           
+            resultCell.tilteLabel.text = "自動車税"
+            
+            return resultCell
+            
+         case 3:
+            guard let weightTaxCell = tableView.dequeueReusableCell(withIdentifier: WeightTaxTableViewCell.reusableIdentifier, for: indexPath) as? WeightTaxTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            return weightTaxCell
+            
+         case 4:
+            guard let resultCell = tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.reusableIdentifier, for: indexPath) as? ResultTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            resultCell.tilteLabel.text = "重量税"
+            
+            return resultCell
+            
+        default:
+            return UITableViewCell()
+        }
+        
     }
     
 }
