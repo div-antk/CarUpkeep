fcc4463e5f14f793e0b3162e35fcd7e3d46b6945
div-antk <51283468+owaii@users.noreply.github.com>
2022-01-19 23:50:33 +0900

    車両価格のカスタムセルを作成

---
+++ CarUpkeep/Cell/PriceTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PriceTableViewCell.swift
+//  CarUpkeep
+//
+//  Created by Takuya Ando on 2022/01/19.
+//
+
+import UIKit
+import Instantiate
+import InstantiateStandard
+
+class PriceTableViewCell: UITableViewCell, Reusable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
