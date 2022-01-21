e4038956ec4a2ffbeb235ec16324e6705bc0e6b9
div-antk <51283468+owaii@users.noreply.github.com>
2022-01-21 20:52:52 +0900

    VCに必要なセルを登録

--- CarUpkeep/PriceViewController.swift
+++ CarUpkeep/PriceViewController.swift
@@ -10,8 +10,64 @@ import Instantiate
 
 class PriceViewController: UIViewController, StoryboardInstantiatable {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 270
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        // カスタムセルを登録
+        tableView.register(UINib(nibName: TitleTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: TitleTableViewCell.reusableIdentifier)
+        
+        tableView.register(UINib(nibName: PriceTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: PriceTableViewCell.reusableIdentifier)
+        
+        tableView.register(UINib(nibName: ResultTableViewCell.reusableIdentifier, bundle: nil), forCellReuseIdentifier: ResultTableViewCell.reusableIdentifier)
+        
+    }
+}
+
+extension PriceViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let titleCell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.reusableIdentifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        switch indexPath.item {
+        case 0:
+            titleCell.titleLabel.text = "*車両価格"
+            titleCell.subtitleLabel.text = "ローンがある場合設定してください"
+            
+            return titleCell
+            
+        case 1:
+            guard let priceCell = tableView.dequeueReusableCell(withIdentifier: PriceTableViewCell.reusableIdentifier, for: indexPath) as? PriceTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            return priceCell
+            
+        case 2:
+            guard let resultCell = tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.reusableIdentifier, for: indexPath) as? ResultTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            resultCell.tilteLabel.text = "*ローン"
+            
+            return resultCell
+            
+        default:
+            return UITableViewCell()
+        }
     }
 }
