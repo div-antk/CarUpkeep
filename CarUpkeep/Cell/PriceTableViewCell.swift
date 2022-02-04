b4ea25429e447da84fda51a6641367747ba18c7f
div-antk <51283468+owaii@users.noreply.github.com>
2022-02-04 18:33:06 +0900

    textFieldの繋ぎこみで値取得の実験

--- CarUpkeep/Cell/PriceTableViewCell.swift
+++ CarUpkeep/Cell/PriceTableViewCell.swift
@@ -16,13 +16,24 @@ class PriceTableViewCell: UITableViewCell, Reusable {
     @IBOutlet weak var installmentsTextField: UITextField!
     @IBOutlet weak var interestTextField: UITextField!
     
+    var price: Int?
+    var installments: Int = 0
+    var interest: Int = 0
+    
     override func awakeFromNib() {
         super.awakeFromNib()
 
         bgLayerView.layer.borderWidth = 2
         bgLayerView.layer.borderColor = UIColor.white.cgColor
+        
+        installments = Int(installmentsTextField.text ?? "0") ?? 0
+        interest = Int(interestTextField.text ?? "0") ?? 0
+        
     }
 
+    @IBAction func priceTextField(_ sender: UITextField) {
+        price = sender.textToInt
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
